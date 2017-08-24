353441cc2d51367e10c49a69248c7b98dcd0dd2e
codden <caroline@codden.no>
2017-08-24 16:09:38 +0200

    init react webpage

---
+++ src/js/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function Header() {
+    return (
+        <div>
+            <div className="header">
+                <h2>Caroline Odden</h2>
+            </div>
+            <p className="menu menu-flexbox">
+                <div className="menu-item">Heyo</div>
+                <div className="menu-item">Hoii</div>
+            </p>
+        </div>
+    )
+}
---
+++ src/js/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export default function Home() {
+    return (
+        <p className="body">
+            På denne siden har jeg planer om å legge prosjekter jeg jobber med.
+            I første omgang vil jeg begynne med et React JS prosjekt, og
+            etterhvert begynne med Elm JS. Skulle du være på siden her å
+            finne noe bugs eller har forslag til forbedringer, kan du
+            gjerne sende mail til <code>caroline@codden.no</code>, eller
+            ta en titt på <code>https://github.com/caroline-odden/codden</code>
+            &nbsp;og legg igjen kommentarer!
+            <br />
+            <br />
+            Happy hacking!
+        </p>
+    )
+}
